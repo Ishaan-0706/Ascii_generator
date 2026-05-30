c8798d5d507d4396cc5872050540198e3a04e7f8
Ishaan-0706 <ishaanparikh0706@gmail.com>
2026-05-30 11:43:16 +0530

    error handle

--- src/modes/utils/mod.rs
+++ src/modes/utils/mod.rs
@@ -10,5 +10,20 @@ use anyhow::Error;
 pub fn colorize(s: &str , color: &str) -> String{
     match color.to_lowercase().as_str(){
         "red" => s.red().bold().to_string(),
+                "blue" => s.blue().bold().to_string(),
+                "white" => s.white().bold().to_string(),
+        "green" => s.green().bold().to_string(),
+        "cyan" => s.cyan().bold().to_string(),
+        "yellow" => s.yellow().bold().to_string(),
+
+        _ => s.white().bold().to_string(),
+    }
+}
+
+pub fn print_error(e: &Error){
+    eprintln!("{}" "{}", "Error: ".red().bold() , e);
+
+    for cause in e.chain().skip(1){
+        eprintln!("{}" "{}", "Caused by: ".red().bold() , cause);
     }
 }
